1911f739f12a8e800be31f9529d2c073adadd7a0
muchtom <muchemwatatendab@gmail.com>
2023-05-08 15:26:06 +0200

    First Commit Fleet Management System

---
+++ src/main/java/com/fleetmanagementsystem/fms_server/FmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.fleetmanagementsystem.fms_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fleetmanagementsystem/fms_server/FmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleetmanagementsystem.fms_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
